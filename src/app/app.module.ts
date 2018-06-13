6b7aaae99b13ba80c890373e70a3481264b03814
HersonCastillo <30848872+HersonCastillo@users.noreply.github.com>
2018-06-12 18:38:16 -0600

    add grid bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -25,7 +26,8 @@ const appRoutes: Routes = [
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        RouterModule.forRoot(appRoutes)
+        RouterModule.forRoot(appRoutes),
+        MaterialModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,81 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } 
+from '@angular/platform-browser/animations';
+
+import { 
+  MatButtonModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatDividerModule,
+  MatListModule,
+  MatTabsModule,
+  MatCardModule,
+  MatChipsModule,
+  MatDialogModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatSnackBarModule,
+  MatStepperModule,
+  MatTooltipModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSelectModule,
+  MatExpansionModule
+} from '@angular/material';
+
+import { MatProgressBarModule } 
+from '@angular/material/progress-bar';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDividerModule,
+    MatListModule,
+    MatTabsModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatProgressBarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSnackBarModule,
+    MatStepperModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSelectModule,
+    MatExpansionModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDividerModule,
+    MatListModule,
+    MatTabsModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatProgressBarModule,
+    MatDatepickerModule,
+    MatSnackBarModule,
+    MatStepperModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSelectModule,
+    MatExpansionModule
   ],
   declarations: []
 })
