d72f2295bc991d8cb0445171c40517a5977345aa
HersonCastillo <30848872+HersonCastillo@users.noreply.github.com>
2018-06-19 18:58:36 -0600

    changes

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
-  selector: 'app-error',
-  templateUrl: './error.component.html',
-  styleUrls: ['./error.component.css']
+    selector: 'app-error',
+    templateUrl: './error.component.html',
+    styleUrls: ['./error.component.css']
 })
-export class ErrorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ErrorComponent {
+    constructor(
+        private router: Router
+    ){}
+    goTo(route: string){
+        this.router.navigate([route]);
+    }
 }
